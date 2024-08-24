dcdc5246fd28746025bd6e88e53cc83d404857b7
Misha Grinko <glukym@gmail.com>
2024-08-24 14:09:17 +0300

    MovieSearch

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <script setup>
-// import movies from './data/movies.json';
+import { ref, computed } from 'vue';
+import movies from './data/movies.json';
+
+const query = ref('');
+const normalizedQuery = computed(() => query.value.toLowerCase());
+const visibleMovies = computed(() => {
+  return movies.filter(m => {
+    return `${m.title}\n${m.description}`
+      .toLowerCase()
+      .includes(normalizedQuery.value);
+  });
+});
 </script>
 
 <template>
@@ -15,13 +26,50 @@
               id="search-query"
               class="input"
               placeholder="Type search word"
+              v-model.trim="query"
             />
           </div>
         </div>
       </div>
 
       <div class="movies">
-        <!-- Copy movies here -->
+        <div
+          class="card"
+          data-cy="movie"
+          v-for="movie of visibleMovies"
+          :key="movie.imdbId"
+        >
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img
+                data-cy="movie__image"
+                :src="movie.imgUrl"
+                :alt="movie.title"
+              />
+            </figure>
+          </div>
+
+          <div class="card-content">
+            <div class="media">
+              <div class="media-left">
+                <figure class="image is-48x48">
+                  <img src="./assets/images/imdb-logo.jpeg" alt="imdb" />
+                </figure>
+              </div>
+
+              <div class="media-content">
+                <p class="title is-8" data-cy="movie__title">
+                  {{ movie.title }}
+                </p>
+              </div>
+            </div>
+
+            <div class="content">
+              <p data-cy="movie__description">{{ movie.description }}</p>
+              <a :href="movie.imdbUrl" data-cy="movie__link">IMDB</a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
 
